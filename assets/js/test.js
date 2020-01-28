e92b1ead6bbfb443a2344fd8ebd73a376485cd0f
iwanaminami <iwanamishoya@gmail.com>
2020-01-28 11:19:08 +0900

    update

---
+++ assets/js/test.js
@@ -0,0 +1,5 @@
+var testtext = $("#aaaa").prev().html();
+//alert(testtext);
+document.write('<h1>' + testtext + 'こんにちは</h1>');
+var testid = $(this).prev().html();
+document.write('<p><i>' + testid + '</i></p>');
